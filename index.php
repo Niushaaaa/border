c569ada950a777596d520bb8c58d613f4a5537ee
dianoush <ebrahimpoordia@gmail.com>
2022-11-07 00:51:25 +0330

    اضافه كردن سطر و ستون
    اضافه كردن سطر و ستون به قالب جدول

--- index.php
+++ index.php
@@ -7,9 +7,14 @@
 <table border="5">
     <?php
     for ($a = 1; $a <= 10; $a++) {
+        echo "<tr>";
         for ($b = 1; $b <= 10; $b++) {
+            echo "<td>";
             echo $a * $b;
+            echo "</td>";
         }
+        echo "</br>";
+        echo "</tr>";
     }
     ?>
 </table>
