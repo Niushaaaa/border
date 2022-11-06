6ac7383dc87c2131ff35b9b997d000c86996c184
dianoush <ebrahimpoordia@gmail.com>
2022-11-07 00:29:41 +0330

    نوشتن كد جدول
    در اينجا كار كد هاي  اچ تي ام ال هد تايتل بادي و تيبل رو اضافه مي كنيم براي گرفتن قالب كلي جدول

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<?php
-for ($a = 1; $a <= 10; $a++) {
-    for ($b = 1; $b <= 10; $b++) {
-        echo $a * $b;
+<html>
+<head>
+    <title>
+    </title>
+</head>
+<body>
+<table>
+    <?php
+    for ($a = 1; $a <= 10; $a++) {
+        for ($b = 1; $b <= 10; $b++) {
+            echo $a * $b;
+        }
     }
-}
+    ?>
+</table>
+</body>
+</html>
+    
